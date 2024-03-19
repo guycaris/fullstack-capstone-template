be99bd812c123e1179d36d58bf2c083c7332b0fd
guycaris <guycaris@yahoo.com>
2024-03-19 19:56:38 +0000

    Second commit

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -38,7 +38,7 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
