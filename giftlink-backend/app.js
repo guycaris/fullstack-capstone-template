4b30c85850b99d07f07983d3dec97342837b3524
guycaris <guycaris@yahoo.com>
2024-03-20 21:49:48 +0000

    Working commit

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -6,8 +5,6 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-
-
 
 
 const app = express();
@@ -28,7 +25,7 @@ app.use(express.json());
 const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require('./routes/searchRoutes');
+//{{insert code here}}
 
 
 const pinoHttp = require('pino-http');
@@ -38,10 +35,10 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('api/gifts', giftRoutes);
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/search', searchRoutes);
+//{{insert code here}}
 
 
 // Global Error Handler
