453170627d0d42dbe41eb35c66a4cc0802edd303
guycaris <guycaris@yahoo.com>
2024-03-22 15:13:09 +0000

    Update user details

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -9,6 +9,8 @@ dotenv.config();
 
 const logger = pino();  // Create a Pino logger instance
 
+const { body, validationResult } = require('express-validator');
+
 //Create JWT secret
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -105,6 +107,56 @@ router.post('/login', async (req, res) => {
         logger.error(e);
         return res.status(500).json({ error: 'Internal server error', details: e.message });
       }
+});
+
+
+router.put('/update', async (req, res) => {
+    
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+try {
+    // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
+    const email = req.headers.email;
+
+    if (!email) {
+        logger.error('Email not found in the request headers');
+        return res.status(400).json({ error: "Email not found in the request headers" });
+    }
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+
+    // Task 5: find user credentials in database
+    const existingUser = await collection.findOne({ email });
+
+    existingUser.updatedAt = new Date();
+
+    // Task 6: update user credentials in database
+    const updatedUser = await collection.findOneAndUpdate(
+        { email },
+        { $set: existingUser },
+        { returnDocument: 'after' }
+    );
+
+    // Task 7: create JWT authentication using secret key from .env file
+    const payload = {
+        user: {
+            id: updatedUser._id.toString(),
+        },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+
+    res.json({authtoken});
+    
+} catch (e) {
+     return res.status(500).send('Internal server error');
+
+}
 });
 
 module.exports = router;
